70b0ca6ccb0ec3ae893acb93cc818e7089f8d54e
Jye Robinson <jye@jrobi.co>
2020-09-24 11:19:23 +1000

    Changed server.js to use MongoDB for storage

--- server/server.js
+++ server/server.js
@@ -4,35 +4,12 @@ var cors = require('cors');
 const http = require('http').Server(app);
 const server = require('./listen.js');
 const bodyParser = require('body-parser');
-const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
 
 const PORT=3000;
-
-//USER ROUTES
-require('./routes/authroute.js')(app, path);
-require('./routes/newuserroute.js')(app, path);
-require('./routes/deleteuserroute.js')(app, path);
-require('./routes/getusersroute.js')(app, path);
-require('./routes/getusersgroupsroute.js')(app, path);
-
-//GROUP ROUTES
-require('./routes/newgrouproute.js')(app, path);
-require('./routes/deletegrouproute.js')(app, path);
-require('./routes/getgroupsroute.js')(app, path);
-require('./routes/addusertogrouproute.js')(app, path);
-require('./routes/deleteuserfromgrouproute.js')(app, path);
-require('./routes/addnewadminroute.js')(app, path);
-require('./routes/addnewassisroute.js')(app, path);
-require('./routes/deladminroute.js')(app, path);
-require('./routes/delassisroute.js')(app, path);
-
-//CHANNEL ROUTES
-require('./routes/newchannelroute.js')(app, path);
-require('./routes/deletechannelroute.js')(app, path);
-require('./routes/addusertochannelroute.js')(app, path);
-require('./routes/deluserfromchannelroute.js')(app, path);
-
+const mongoUrl = 'mongodb://localhost:27017';
 
 app.use(cors());
 
@@ -40,5 +17,35 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname + '/../dist/assignment')));
 
-server.listen(http, PORT);
+MongoClient.connect(mongoUrl, {poolSize:10, useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+    if (err) {return console.log(err)}
+    const db = client.db('ass2')
+    //USER ROUTES
+    require('./routes/authroute.js')(app, db);
+    require('./routes/newuserroute.js')(app, db);
+    require('./routes/deleteuserroute.js')(app, db);
+    require('./routes/getusersroute.js')(app, db);
+    require('./routes/getusersgroupsroute.js')(app, db);
+
+    //GROUP ROUTES
+    require('./routes/newgrouproute.js')(app, db);
+    require('./routes/deletegrouproute.js')(app, db);
+    require('./routes/getgroupsroute.js')(app, db);
+    require('./routes/addusertogrouproute.js')(app, db);
+    require('./routes/deleteuserfromgrouproute.js')(app, db);
+    require('./routes/addnewadminroute.js')(app, db);
+    require('./routes/addnewassisroute.js')(app, db);
+    require('./routes/deladminroute.js')(app, db);
+    require('./routes/delassisroute.js')(app, db);
+
+    //CHANNEL ROUTES
+    require('./routes/newchannelroute.js')(app, db);
+    require('./routes/deletechannelroute.js')(app, db);
+    require('./routes/addusertochannelroute.js')(app, db);
+    require('./routes/deluserfromchannelroute.js')(app, db);
+
+    server.listen(http, PORT);
+
+});
+
 
